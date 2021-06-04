cd88d50dc556c34105b21da9af02b61771120a08
FelipeSanguinetti <Fsanguinetti@udesa.edu.ar>
2021-06-04 15:22:48 -0300

    cambios

--- detalle-cancion/js/detalle-cancion.js
+++ detalle-cancion/js/detalle-cancion.js
@@ -1,14 +1,26 @@
 
 let url="https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/3135556" 
+// https://cors-anywhere.herokuapp.com/corsdemo
 
 fetch(url)
-    .then(
-        function(response){
+    
+    .then(function(response){
         return response.json();
     })
     
-    .then(function(info){
+    .then(function(data){
         
+        console.log(data);
+        let Info=data.data
+        let lista= document.querySelector('.lista')
+        let contenidoLista=''; // Poner el contenido a mostrar dentro de esta lista
+
+        for(let i=0; i<Info.length; i++){
+            contenidoLista += `<li>${Info[i].data.title}</li>`
+        }
+
+        lista.innerHTML += contenidoLista 
+
     })
 
     .catch(function(error){
