016be246a8a718933dbbd36225c17b2a7c7a25f8
delfinadelgui <80966598+delfinadelgui@users.noreply.github.com>
2021-06-24 18:33:40 -0300

    cambios

--- js/detalle-artista.js
+++ js/detalle-artista.js
@@ -18,7 +18,39 @@ fetch(url)
     .then(function(data){
 
         console.log(data);
-        
+        window.addEventListener("load", function(){
+
+
+            let queryString= location.search
+            let QSobject= new URLSearchParams(queryString)
+            
+            let id=QSobject.get('q')
+            
+            let url= `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/album?q=${id}`
+            
+            fetch(url)
+            
+                .then(function(response){
+                     return response.json()
+            
+                })
+            
+                .then(function(data){
+            
+                   
+            
+            
+                })
+            
+                
+                .catch(function(error){
+                    
+                    console.log(error);
+                
+                })
+            
+            
+            })
 
 
 
